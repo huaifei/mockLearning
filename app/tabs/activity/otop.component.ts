07dc56cf71204759bf355b02b95a3a68b7e3d01c
Huaifei Zhang <huaifei.zhang@accenture.com>
2017-01-13 10:57:27 +0800

    add img

---
+++ app/tabs/activity/otop.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by huaifei.zhang on 2017/1/13.
+ */
+import {Component } from '@angular/core';
+
+@Component({
+  selector:'atop-part',
+  template:`
+
+
+`,
+
+})
+
+
+export class AtopComponent {
+
+}
